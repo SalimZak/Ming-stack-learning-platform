e951e490ec8488733b07d61c7e7da727ee077a8c
BenjiSerdar <boberpravofc@gmail.com>
2026-04-30 18:32:36 +0200

    Fikset mappe struktur

--- front/web-demo/dist/real-code/js/MC_Grafana.js
+++
@@ -1,9 +0,0 @@
-// Grafana task 1
-
-function grafanaT1Start(){
-  infoBoxText("Dette er en Multiple Choice oppgave for MQTT. Velg riktig svar og lever når du føler deg ferdig. Lykke til!");
-  document.getElementById('grafanaT1-infobox').style.display='none';
-  document.getElementById('grafanaT1-taskbox').style.display='block';
-  selectedAnswersMC = [];
-  taskFinishedMC = false;
-}
